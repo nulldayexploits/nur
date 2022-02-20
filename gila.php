3a4dda67d7ac642b61337045a6f4666bfe56b60e
nulldayexploits <nulldayexploits1117@gmail.com>
2022-02-21 00:44:05 +0800

    ok

---
+++ gila.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Pencarian Cerpen</title>
+
+    <!-- bootstrap 5 css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  
+  <!-- data tables -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
+
+ 
+</head>
+<body>
+
+
+
+
+
+ 
+    <main>
+      <div class="container mt-4">
+
+
+
+    <div class="datatable">
+      <table id="example" class="table table-striped" style="width:100%">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Peristiwa</th>
+                <th>Tanggal Dan Waktu</th>
+                <th>Pelapor</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+                           
+              
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>No</th>
+                <th>Peristiwa</th>
+                <th>Tanggal Dan Waktu</th>
+                <th>Pelapor</th>
+                <th>Status</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+
+
+    </main>
+  </div>
+
+  <!-- bootstrap js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ 
+  <!-- data tables -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 <html>
 <head>
 	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Pencarian Cerpen</title>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <!-- bootstrap 5 css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+	
+	<!-- data tables -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
+
 	<style type="text/css">
 		
 		body{
@@ -47,6 +59,10 @@
 		<a href="login.php" class="button">LOGIN</a>
     </form>
 </center>
+
+
+    <main>
+      <div class="container mt-4">
 
 
 <?php
@@ -103,13 +119,17 @@ function tampildata($query, $data)
     echo "<br><br>
 		  <b style='margin-left:150px;font-size:20px;'>Kata Kunci Yang Dimasukkan: <i><u>$query</u></i></b>
 		  <center>
-		  <table class='w3-table-all' style='width:80%'>
-			<tr class='w3-green'>
+          <div class='datatable'>
+          <table id='example' class='table table-striped' style='width:100%'>
+            <thead>
+			<tr>
 			  <th>No</th>
 			  <th>Judul</th>
 			  <th>Nilai VSM</th>
 			  <th>Aksi</th>
-			</tr>";
+			</tr>
+			</thead>
+			<tbody>";
 
 
     foreach($data as $d){
@@ -134,7 +154,18 @@ function tampildata($query, $data)
     }
     
 
-    echo "</table>";
+    echo "</tbody>
+          <tfoot>  
+            <tr>
+			  <th>No</th>
+			  <th>Judul</th>
+			  <th>Nilai VSM</th>
+			  <th>Aksi</th>
+			</tr>
+		  </tfoot>
+    	</table>
+      </div>";
+
 
 
 }
@@ -148,5 +179,29 @@ if(isset($_POST['submit'])){
 
 
 ?>
+
+
+
+    </main>
+  </div>
+
+  <!-- bootstrap js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ 
+  <!-- data tables -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+  
+  <script type="text/javascript">
+  	
+	$(document).ready(function() {
+	    $('#example').DataTable( {
+	        "order": [[ 2, "desc" ]]
+	    });
+    });
+
+  </script>
+
 </body>
 </html>
